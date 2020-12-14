4e695bc689f4f2ebf84239b4f853ebe634ec54a1
Wmt-Monica <3040988158@qq.com>
2020-12-14 18:54:36 +0800

    冒泡排序练习
    Signed-off-by: Wmt-Monica <3040988158@qq.com>

---
+++ src/Sort/ButtonSort/buttonSortLearn.java
@@ -0,0 +1,27 @@
+package Sort.ButtonSort;
+
+import java.util.Arrays;
+
+public class buttonSortLearn {
+    public static void main(String[] args) {
+        int[] array = {-1,-5,6,3,8,0,-4,7,6,8,9};
+        int step;
+        boolean flag = false;
+        for (int i = 0; i < array.length-1; i ++){
+            for (int j = 0; j < array.length-1-i; j ++){
+                if (array[j] > array[j+1]){
+                    step = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = step;
+                    flag = true;
+                }
+            }
+            if (flag == false){
+                break;
+            }else {
+                flag = false;
+                System.out.println("第"+(i+1)+"趟排序结果："+ Arrays.toString(array));
+            }
+        }
+    }
+}
