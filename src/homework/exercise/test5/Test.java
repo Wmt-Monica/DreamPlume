0a0cdf33abf3296e8bcd8565206bea8326358151
Wmt-Monica <3040988158@qq.com>
2020-12-23 09:44:25 +0800

    2020/12/23
    Signed-off-by: Wmt-Monica <3040988158@qq.com>

--- src/homework/exercise/test5/Test.java
+++ src/homework/exercise/test5/Test.java
@@ -31,6 +31,22 @@ public class Test {
         root.backPrintTreeNode(root);
         System.out.println("\n============层序遍历============");
         root.cenPrintTreeNode(root);
+
+        TreeNode root2 = new TreeNode("A");
+        TreeNode nodeB2 = new TreeNode("B");
+        TreeNode nodeC2 = new TreeNode("C");
+        TreeNode nodeD2 = new TreeNode("D");
+        TreeNode nodeE2 = new TreeNode("E");
+        TreeNode nodeF2 = new TreeNode("F");
+        TreeNode nodeG2 = new TreeNode("G");
+        root2.setLeft(nodeB2);
+        root2.setRight(nodeC2);
+        nodeB2.setLeft(nodeD2);
+        nodeC2.setLeft(nodeE2);
+        nodeC2.setRight(nodeF2);
+        nodeD2.setRight(nodeG2);
+        boolean flag = root.isSameBTree(root,root2);
+        System.out.println("\n两棵树是否相同 = "+flag);
     }
 }
 class TreeNode{
@@ -102,6 +118,10 @@ class TreeNode{
         System.out.print(root.data+" ");
     }
 
+    /**
+     * 层序遍历
+     * @param root 根节点
+     */
     public void cenPrintTreeNode(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
@@ -113,7 +133,36 @@ class TreeNode{
             }
             if (treeNode.right != null) {
                 queue.add(treeNode.right);
+                System.out.println();
             }
         }
+    }
+
+
+    /**
+     * 判断两颗二叉树是否相同（结构相同且对应结点的数据相同）
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean isSameBTree(TreeNode root1, TreeNode root2) {
+        // 一：判断两棵树的结构是否相同
+        // 1):该节点都为 null 时该处结构相同（该节点可以是根节点，左节点，右节点）
+        if (root1 == null && root2 == null) {
+            return true;
+        }else if (root1 == null) {
+            // 2):该节点处，只有 root1 为 null, root2 不为 null,说明该处两棵树的结构不相同，返回 false
+            return false;
+        }else if (root2 == null) {
+            // 3):该节点处，只有 root2 为 null, root1 不为 null,说明该处两棵树的结构不相同，返回 false
+            return false;
+        }
+
+        // 二：判断两棵树的该处节点的数据是否相同
+        // 1):当该处节点两棵树都不为 null 则判断其该节点的数据是否相同
+        return (root1.data == root2.data) &&
+                // 2):使用递归接着判断 root1 和 root2 的左子树和右子树是否相同
+                (isSameBTree(root1.left,root2.left) && isSameBTree(root1.right,root2.right));
     }
 }
