2343fb8ccf91420c5d0e06fa062ea89b4cd207da
Wmt-Monica <3040988158@qq.com>
2020-12-26 10:47:42 +0800

    2020/12/26
    Signed-off-by: Wmt-Monica <3040988158@qq.com>

--- src/homework/exercise/test8/BinaryTree.java
+++ src/homework/exercise/test8/BinaryTree.java
@@ -1,10 +1,16 @@
 package homework.exercise.test8;
+/**
+ * 8、请在 BinaryTree 的 isSameBTree 和 getNodeNumIthLevel 方法中补充代码，并且在 main 方
+ *    法中完成测试。（出题人：胡志成）
+ */
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class BinaryTree{
-    BTreeNode root ;
+    private BTreeNode root ;
 
     public BinaryTree(char[] charArray) {
         root = createBinaryTree(charArray, 0);
@@ -15,19 +21,29 @@ class BinaryTree{
     public static void main(String[] args) {
         char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
         BinaryTree binaryTree1 = new BinaryTree(charArray);
-        System.out.print("The node num of level 2 is:");
+        System.out.println("===========层序遍历============");
+        List<List<Character>> list = binaryTree1.sequencePrint(binaryTree1.getRoot());
+        for (List<Character> list1 : list) {
+            for (Character i : list1) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("=============================");
+
+        System.out.print("The node num of level 2 is:");  // 第二层总节点数
         int num = getNodeNumIthLevel(binaryTree1.getRoot(), 2);
         System.out.println(num);
         System.out.print("The node num of level 5 is:");
-        int num2 = getNodeNumIthLevel(binaryTree1.getRoot(), 5);
+        int num2 = getNodeNumIthLevel(binaryTree1.getRoot(), 3);  // 第三层节点总数
         System.out.println(num2);
 
         char[] charArray2 = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j'};
         BinaryTree binaryTree2 = new BinaryTree(charArray2);
-        System.out.print("binaryTree1 equals to binaryTree2:");
+        System.out.print("binaryTree1 equals to binaryTree2:");  // 判断 binaryTree1 是否与 binaryTree2 具有相等
         boolean equalOrNot = isSameBTree(binaryTree1.getRoot(), binaryTree2.getRoot());
         System.out.println(equalOrNot);
-        System.out.print("binaryTree1 equals to binaryTree3:");
+        System.out.print("binaryTree1 equals to binaryTree3:");  // 判断 binaryTree1 是否与 binaryTree3 具有相等
         BinaryTree binaryTree3 = new BinaryTree(charArray);
         boolean equalOrNot2 = isSameBTree(binaryTree1.getRoot(), binaryTree3.getRoot());
         System.out.println(equalOrNot2);
@@ -83,26 +99,55 @@ class BinaryTree{
      * @return
      */
     public static int getNodeNumIthLevel(BTreeNode root, int i) {
-        int floor = 1;  // 树的遍历的层数
+        int floor = 0;  // 树的遍历的层数
         int nums = 0; // 第 i 层的总节点数
         Queue<BTreeNode> queue = new LinkedList<>();
         queue.add(root);
         while (!queue.isEmpty()) {
-            BTreeNode bTreeNode = queue.poll();
-            if (bTreeNode.left != null) {
-                queue.add(bTreeNode.left);
+            int size = queue.size();  // 获取此时遍历层数的总节点数
+            for (int k = 0; k < size; k ++) {  // 循环遍历 queue 中的节点，将其这些节点弹出并添加这些节点的所有子节点
+                BTreeNode bTreeNode = queue.poll();
+                if (bTreeNode.left != null) {
+                    queue.add(bTreeNode.left);
+                }
+                if (bTreeNode.right != null) {
+                    queue.add(bTreeNode.right);
+                }
             }
-            if (bTreeNode.right != null) {
-                queue.add(bTreeNode.right);
-                // 遍历到了右子树说明已经遍历完了一层
-                floor ++;
-            }
+            // 每当遍历完一层就将 floor 层数增加一，表示二叉树层数的下移
+            floor ++;
+            // 如果当 floor == i 说明已经遍历到了第 i 层
             if (floor == i) {
-                nums ++;
+                nums = size;  // 此时将第 i 层的总子节点树赋值给 nums,然后跳出循环
+                break;
             }
         }
         return nums;
     }
+
+    // 层序遍历二叉树
+    public List<List<Character>> sequencePrint(BTreeNode root) {
+        List<List<Character>> list = new ArrayList<>();
+        Queue<BTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Character> list1 = new ArrayList<>();
+            for (int i = 0; i < size; i ++) {
+                BTreeNode bTreeNode = queue.poll();
+                list1.add(bTreeNode.data);
+                if (bTreeNode.left != null) {
+                    queue.add(bTreeNode.left);
+                }
+                if (bTreeNode.right != null) {
+                    queue.add(bTreeNode.right);
+                }
+            }
+            list.add(list1);
+        }
+        return list;
+    }
+
     /**
      * 二叉树结点定义
      */
