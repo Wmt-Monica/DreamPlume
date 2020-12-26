97bbc964edd2dc0ae875dfe452b2a2bd77bda91c
Wmt-Monica <3040988158@qq.com>
2020-12-26 11:00:10 +0800

    2020/12/26
    Signed-off-by: Wmt-Monica <3040988158@qq.com>

--- src/homework/exercise/test8/BinaryTree.java
+++ src/homework/exercise/test8/BinaryTree.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-class BinaryTree{
+    class BinaryTree {
     private BTreeNode root ;
 
     public BinaryTree(char[] charArray) {
